a2e6388addea0aa74fe60d3b9a4693dae6486102
Maximilian Burzer <maxi.burzer@gmail.com>
2023-01-15 15:39:35 +0100

    chnage background

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -8,7 +8,7 @@ import Stats from './Stats';
 import Chat from './Chat';
 import {GameProps} from '../../types/props';
 import {Card} from '@blueprintjs/core';
-import table from '../../assets/table.jpg';
+import background from '../../assets/background.jpg';
 
 export default function Game({playerID}: GameProps) {
     return (
@@ -18,7 +18,7 @@ export default function Game({playerID}: GameProps) {
                 <Chat playerID={playerID} />
             </div>
             <div className="table-wrapper">
-                <Card className="table">
+                <Card className="table" style={{backgroundImage: `url(${background})`}}>
                     <div className="board">
                         <Pile />
                         <Stack />
